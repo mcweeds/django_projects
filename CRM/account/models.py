513846341fea9eaf5a3f68b051bc07e8cc369ef1
Guillermo PC CASA <gfaus91@gmail.com>
2020-05-08 22:41:04 +0200

    DATABASE MODELS RELATIONS AND DEMO QUERIES

--- CRM/account/models.py
+++ CRM/account/models.py
@@ -10,27 +10,37 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     CATEGORY = (
-            ('Indoor','Indoor'),
-            ('Out Door','Out Door'),
+            ('Indoor', 'Indoor'),
+            ('Out Door', 'Out Door'),
             )
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description = models.CharField(max_length=500, null=True)
+    description = models.CharField(max_length=500, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-
+    tags = models.ManyToManyField(Tag)
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
     STATUS = (
-            ('Pending','Pending'),
-            ('Out of delivery','Out of delivery'),
-            ('Delivered','Delivered'),
-            )
-    #customer =
-    #product =
+        ('Pending', 'Pending'),
+        ('Out of delivery', 'Out of delivery'),
+        ('Delivered', 'Delivered'),
+        )
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-    status = models.CharField(max_length=100, null=True)
+    status = models.CharField(max_length=100, null=True, choices=STATUS)
+
+
+
