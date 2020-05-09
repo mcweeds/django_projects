6a3c54430b63f9ee41067d6067c6db79db715918
Guillermo PC CASA <gfaus91@gmail.com>
2020-05-09 23:02:43 +0200

    In URL Routing and Dinamics Templates

--- CRM/account/views.py
+++ CRM/account/views.py
@@ -2,13 +2,27 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 # Create your views here.
+from .models import *
+
 
 def home(request):
-    return render(request,'account/dashboard.html')
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+
+    context = {'orders': orders, 'customers': customers, 'total_customers': total_customers,
+               'total_orders': total_orders, 'delivered': delivered, 'pending': pending}
+
+    return render(request, 'account/dashboard.html', context)
 
 
 def products(request):
-    return render(request, 'account/products.html')
+    products = Product.objects.all()
+    return render(request, 'account/products.html', {'products': products})
 
-def customer(request):
+def customer(request,pk_test):
+    customer = Customer.objects.get(id=pk_test)
     return render(request, 'account/customer.html')
